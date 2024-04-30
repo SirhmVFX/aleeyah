5344a10285bb0919301283973a8ea97b9f0d87e3
The Fullstack Mechanic <111003107+SirhmVFX@users.noreply.github.com>
2024-04-30 21:23:39 +0100

    Update page.jsx

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -32,7 +32,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:8000/auth/login",
+        "https://aleeyah-api.vercel.app/auth/login",
         formData,
       );
       console.log(response?.data);
