1331e354a01a7bec69afd20f4da6f6714be51c04
belloibrahv <belloibrahv@gmail.com>
2024-05-01 01:59:28 +0100

    refined 4-5

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ let currentChatSession; // Variable to store the chat session
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://aleeyah.vercel.app/",
   })
 );
 app.use(express.json());
