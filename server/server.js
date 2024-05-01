5807d555bd438229b7208ae084f3d28b3e2ffecc
The Fullstack Mechanic <111003107+SirhmVFX@users.noreply.github.com>
2024-05-01 13:45:20 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ let currentChatSession;
 
 app.use(
   cors({
-    origin: "https://aleeyah.vercel.app",
+    origin: "https://aleeyah-beta.vercel.app",
     methods: ["POST", "GET"],
     credentials: true
   })
