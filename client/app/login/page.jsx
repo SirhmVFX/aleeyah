a77dd4f664b8fadb858bf4f72bce21283da908aa
The Fullstack Mechanic <111003107+SirhmVFX@users.noreply.github.com>
2024-05-01 05:47:54 +0100

    Update page.jsx

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -38,13 +38,14 @@ export default function Login() {
       console.log(response?.data);
       setUserToken(response?.data?.token);
       setUserId(response?.data?.user?.id); // Optional: Store token in local storage
-      setUsername(response?.data?.user?.username); // Optional: Store token in local storage
+      setUsername(response?.data?.user?.username)
+      // Optional: Store token in local storage
       toast.success(response.data.message, {
         position: "top-center",
       });
 
       setTimeout(() => {
-        router.push(` /${userId}/bot`); // Redirect to login page after successful signup
+        router.push(`/${userId}/bot`); // Redirect to login page after successful signup
       }, 3000);
       // Redirect to dashboard after successful login
     } catch (error) {
@@ -131,7 +132,7 @@ export default function Login() {
           </div>
           {formData.password.length > 6 && formData.email.length > 0 ? (
             loading ? (
-              <button className="bg-green-200 p-4 md:p-6 rounded text-white">
+              <button className="bg-green-200 flex justify-center items-center p-4 md:p-6 rounded text-white">
                 <AiOutlineLoading3Quarters className="text-primarycolor font-bold animate-spin" />
               </button>
             ) : (
